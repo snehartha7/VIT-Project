b5ae253679207fc567a8435d51c7b931004baa3e
snehartha7 <snehartha7@gmail.com>
2026-09-14 11:56:21 +0530

    Add new code

---
+++ main.py
@@ -0,0 +1,70 @@
+student_grades ={}
+
+def add_student(name, grade):
+    student_grades[name] = grade
+    print(f"Added {name} with grade {grade}")
+    
+    
+def get_student(name):
+    if name in student_grades:
+        print(f"{name}: {student_grades[name]}")
+    else:
+        print(f"Student {name} not found.")
+        
+def update_student(name, grade):
+    if name in student_grades:
+        student_grades[name] = grade
+        print(f"Updated {name} to grade {grade}")
+    else:
+        print(f"Student {name} not found.")
+        
+def delete_student(name):
+    if name in student_grades:
+        del student_grades[name]
+        print(f"Deleted {name}")
+    else:
+        print(f"Student {name} not found.")
+        
+def display_all_students():
+    if student_grades:
+        for name, grade in student_grades.items():
+            print(f"{name}: {grade}")
+    else:
+        print("No students found.")
+        
+def main():
+    while True:
+        print("\n Student Grades Management System")
+        print("1. Add Student")
+        print("2. Get Student")
+        print("3. Update Student")
+        print("4. Delete Student")
+        print("5. Display All Students")
+        print("6. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            name = input("Enter student name: ")
+            grade = input("Enter student grade: ")
+            add_student(name, grade)
+        elif choice == '2':
+            name = input("Enter student name: ")
+            get_student(name)
+        elif choice == '3':
+            name = input("Enter student name: ")
+            grade = input("Enter new grade: ")
+            update_student(name, grade)
+        elif choice == '4':
+            name = input("Enter student name: ")
+            delete_student(name)
+        elif choice == '5':
+            display_all_students()
+        elif choice == '6':
+            print("Exiting the program.")
+            print("Thanks for using the Student Grades Management System!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+main()
